9191483daf7c96833b0f25352d4eefb058dea847
Rebecca Kaplan <beccatess@gmail.com>
2017-09-14 23:57:47 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,5 +2,5 @@
 function paragraphSelector(){return $('p')}
 function lastImageSelector(){return $('img:last')}
 function ninjaBabySelector(){return $('#baby-ninja')}
-function divSelector(){return $('.pics div')}
+function divSelector(){return $('div')}
 function firstListItem(){return $('ul li:first-child')}
